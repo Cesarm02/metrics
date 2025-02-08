9ea84e8236d4faf05d2f43ae14432602c9d653aa
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2025-02-08 01:37:52 -0500

    Uso del compositeMeterRegistry

--- src/main/java/com/metrics/MetricsApplication.java
+++ src/main/java/com/metrics/MetricsApplication.java
@@ -2,13 +2,23 @@ package com.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 public class MetricsApplication {
 	public static void main(String[] args) {
-		MeterRegistry register = new SimpleMeterRegistry();
+		CompositeMeterRegistry compositeMeter = new CompositeMeterRegistry();
 		
-		Counter counter = register.counter("Numero.empleados", "oficina", "Benito Juan");
+		Counter counter = compositeMeter.counter("Numero.empleados", "oficina", "Benito Juan");
+		counter.increment();
+		counter.increment(200);
+		
+		SimpleMeterRegistry registry = new SimpleMeterRegistry();
+		SimpleMeterRegistry registry2 = new SimpleMeterRegistry();
+
+		
+		compositeMeter.add(registry);
+		compositeMeter.add(registry2);
 		counter.increment();
 		counter.increment(200);
 		
